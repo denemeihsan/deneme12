7f7f2fa5b7a73d6921006aebdac94476305eff3a
denemeihsan <altnskihsan2@gmail.com>
2024-05-05 17:23:40 +0300

    HATASIZ

--- src/test/java/stepdefinitions/DB_Stepdefinitions.java
+++ src/test/java/stepdefinitions/DB_Stepdefinitions.java
@@ -20,8 +20,7 @@ public class DB_Stepdefinitions extends Base {
         preparedStatement.setInt(1, id);
         //commit
 
-        resultS
-                et = preparedStatement.executeQuery();
+        resultSet = preparedStatement.executeQuery();
         resultSet.next();
         System.out.println("Database Name Result: " + resultSet.getString("first_name") +
                            "\nDatabase Last Name Result: " + resultSet.getString("last_name"));
